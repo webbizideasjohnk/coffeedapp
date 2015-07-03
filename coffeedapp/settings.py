73af72b874b22e5fe1ddfc360d80db0c1b3ef8c2
John Koch <johnkoch@affordamaccoms-MacBook-Pro.local>
2015-07-03 17:09:26 -0500

    The blog is basically working.

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -15,6 +15,8 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MAIN_DIR = os.path.dirname(os.path.dirname(__file__))
+ON_HEROKU = os.environ.get('ON_HEROKU')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -75,16 +77,17 @@ WSGI_APPLICATION = 'coffeedapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if ON_HEROKU is True:
+    # Parse database configuration from $DATABASE_URL
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config()
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(MAIN_DIR, 'db.sqlite3'),
+        }
     }
-}
-
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -114,3 +117,13 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
  os.path.join(MAIN_DIR, 'static'),
  )
+
+STATIC_ROOT = 'staticfiles'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+AWS_S3_FORCE_HTTP_URL = True
+AWS_QUERYSTRING_AUTH = False
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWSSecretKey')
+AWS_ACCESS_KEY_ID = os.environ.get('AWSAccessKeyId')
+
+AWS_STORAGE_BUCKET_NAME = 'johns1monthpythoncoffeed'
--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+import core.models as coremodels
 # Register your models here.
+admin.site.register(coremodels.Location)
